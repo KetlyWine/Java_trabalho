67070f1d556fa15e8945fd5f726523734c53d65b
Ketly <ketly.wine@gmail.com>
2023-12-19 16:14:14 -0300

    Classe Passageiro criada

---
+++ Companhia_de_Trem/src/model/entities/Passageiro.java
@@ -0,0 +1,60 @@
+package model.entities;
+
+public class Passageiro {
+    private String nome;
+    private String rg;
+    private int idade;
+    private String endereço;
+
+    public Passageiro() {
+    }
+
+    public Passageiro(String nome, String rg, int idade, String endereço) {
+        this.nome = nome;
+        this.rg = rg;
+        this.idade = idade;
+        this.endereço = endereço;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    @Override
+    public String toString() {
+        return "Passageiro{" +
+                "nome='" + nome + '\'' +
+                ", rg='" + rg + '\'' +
+                ", idade=" + idade +
+                ", endereço='" + endereço + '\'' +
+                '}';
+    }
+}
